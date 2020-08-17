7eaca8fc7077b4fa43faf7bdc07a69eed92eaa42
Kwounsu Lee <sb00lgs@gmail.com>
2020-08-17 12:59:05 -0700

    java

--- canFormPalindrome.kt
+++ canFormPalindrome.kt
@@ -17,3 +17,28 @@ fun canFormPalindrome(str: String): Boolean {
 fun main(args: Array<String>) {
     println(canFormPalindrome("edcabbbacd"))
 }
+
+// Java
+/*
+public class Main {
+    public static boolean canFormPalindrome(String str) {
+        List<Character> chars = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (chars.contains(str.charAt(i))) {
+                chars.remove(new Character(str.charAt(i)));
+            } else {
+                chars.add(str.charAt(i));
+            }
+        }
+        if (chars.size() > 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(canFormPalindrome("abbc"));
+    }
+}
+*/
