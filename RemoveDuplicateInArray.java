ddadcba5cabb4d1bc28856fc7528133b317cc158
Kwounsu Lee <sb00lgs@gmail.com>
2020-08-17 09:52:55 -0700

    Update Kotlin code

--- RemoveDuplicateInArray.java
+++ RemoveDuplicateInArray.java
@@ -28,3 +28,33 @@ public class Main {
         }
     }
 }
+
+//Kotlin
+/*
+fun removeDuplicate(arr: IntArray): IntArray {
+    arr.sort()
+    var newArraySize = 1;
+    for (i in 1..arr.size-1) {
+        if (arr[i] != arr[i-1]) {
+            newArraySize++
+        }
+    }
+    val newArray = IntArray(newArraySize)
+    newArray[0] = arr[0]
+    var n = 1
+    for (i in 1..arr.size-1) {
+        if (arr[i] != arr[i-1]) {
+            newArray[n++] = arr[i]
+        }
+    }
+    return newArray
+}
+
+fun main(args: Array<String>) {
+    var arr: IntArray = intArrayOf(4,2,1,5,3,4,2,4)
+    arr = removeDuplicate(arr)
+    for (ele in arr) {
+        println(ele)
+    }
+}
+*/
