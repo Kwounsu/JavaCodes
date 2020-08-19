5f0907d7859c20a3fb07b72f5af349f631e5969f
Kwounsu Lee <sb00lgs@gmail.com>
2020-08-19 14:20:23 -0700

    update java

--- TrappedWater.kt
+++ TrappedWater.kt
@@ -38,3 +38,61 @@ fun trappedWater(N: Int) {
     // Print trapped water
     println (water)
 }
+
+// Java
+/*
+public class Main {
+    public static void main(String[] args) 
+    {
+        missingAndRepeating();
+    }
+    
+    public static void missingAndRepeating() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            
+            // Run tests 'T' times
+            int T = Integer.parseInt(br.readLine());
+            for (int t = 0; t < T; t++) {
+                
+                // Get the size of array N and array through stdin
+                int N = Integer.parseInt(br.readLine());
+                String str = br.readLine();
+
+                // Variables
+                List<Integer> blocks = new ArrayList<Integer>();
+                int water = 0;
+                int left = 0;
+                int right = N-1;
+                int maxLeft = 0;
+                int maxRigth = 0;
+                
+                // Convert String to array
+                for (int i = 0; i < str.length(); i++) {
+                    if (!Character.isWhitespace(str.charAt(i))) {
+                        blocks.add(Character.getNumericValue(str.charAt(i)));
+                    }
+                }
+                
+                // Calculate trapped water
+                while (left < right) {
+                    if (blocks.get(left) <= blocks.get(right)) {
+                        if (blocks.get(left) >= maxLeft) maxLeft = blocks.get(left);
+                        else water += maxLeft - blocks.get(left);
+                        left++;
+                    } else {
+                        if (blocks.get(right) >= maxRigth) maxRigth = blocks.get(right);
+                        else water += maxRigth - blocks.get(right);
+                        right++;
+                    }
+                }
+
+                // Print trapped water
+                System.out.println (water);
+            }
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+    }
+}
+*/
