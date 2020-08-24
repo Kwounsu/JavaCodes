65389f34a9ca36f4cc5ec193a4c5a03047d5d263
Kwounsu Lee <sb00lgs@gmail.com>
2020-08-24 12:47:03 -0700

    Create SubArray.java

---
+++ basic/SubArray.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int[] subarray = Arrays.copyOfRange(arr, i, j+1);
+                int sum = 0;
+                for (int ele: subarray) {
+                    sum += ele;
+                }
+                if (sum < 0) result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
