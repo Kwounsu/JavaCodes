8c3fd47f66749c2323bcc11f7a54a7cdaa7d3758
Kwounsu Lee <sb00lgs@gmail.com>
2020-08-25 14:35:14 -0700

    Update and rename Regex_IP.java to Regex.java

---
+++ simple/Regex.java
@@ -0,0 +1,60 @@
+/*
+ * Find proper IP address using regex
+ */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex {
+    // 0-255 (0-99|000-199|200-249|250-255)
+    String num = "(\\d{1,2}|[0-1]\\d{2}|2[0-4]\\d|25[0-5])";
+    public String pattern = num+"."+num+"."+num+"."+num;
+}
+
+
+/*
+ * Remove duplicate words from string using regex
+ */
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DuplicateWords {
+
+    public static void main(String[] args) {
+
+        String regex = "\\b(\\w+)(?:\\W+\\1\\b)+";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
+        
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+            
+            Matcher m = p.matcher(input);
+            
+            // Check for subsequences of input that match the compiled pattern
+            while (m.find()) {
+                input = input.replaceAll(m.group(), m.group(1));
+            }
+            
+            // Prints the modified sentence.
+            System.out.println(input);
+        }
+        
+        in.close();
+    }
+}
