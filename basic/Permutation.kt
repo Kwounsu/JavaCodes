8db68898b57cde90c6be99420ba7fd39a1233249
Kwounsu Lee <sb00lgs@gmail.com>
2020-10-08 08:02:54 -0700

    Update Permutation.kt

--- basic/Permutation.kt
+++ basic/Permutation.kt
@@ -22,3 +22,20 @@ fun permutations(list: List<Char>): Set<List<Char>> {
     }
     return permutes
 }
+
+// For each element, you have two choices: 
+// either you put it in your subset, or you don't.
+// Therefore, number of subsets will be 2^n
+fun printSubsets(list: List<Char>): Set<List<Char>> {
+    val permutes = mutableSetOf<List<Char>>()
+    for (i in 0..(1 shl list.size)-1) {
+        val subset = mutableListOf<Char>()
+        for (j in 0..list.size-1) {
+            if ((i and (1 shl j)) > 0) {
+                subset.add(list[j])
+            }
+        }
+        permutes.add(subset)
+    }
+    return permutes
+} 
