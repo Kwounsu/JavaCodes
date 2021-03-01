9ccebbdfda0df5249c9b6713d7908e5a82ced401
Kwounsu Lee <sb00lgs@gmail.com>
2021-03-01 18:56:59 +0900

    Update Permutation.java

--- basic/Permutation.java
+++ basic/Permutation.java
@@ -1,16 +1,16 @@
 public class Main {
-  public static void main(String[] args) {
-    int[] arr = {1, 2, 3}; //순열을 만들 배열 
-    int n = arr.length; //배열의 길이 
-    int[] output = new int[n]; //순열 출력을 위한 배열 
-    boolean[] visited = new boolean[n]; //중복해서 뽑지 않기 위해 방문했는지를 체크하는 배열 
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3}; //순열을 만들 배열 
+		int[] output = new int[n]; //순열 출력을 위한 배열 
+		boolean[] visited = new boolean[n]; //중복해서 뽑지 않기 위해 방문했는지를 체크하는 배열 
+		int r = 3; //구하고 싶은 순열크기
 
-    System.out.println("-------- 1. Swap ---------");
-    per1(arr, 0, n, 3);
+		System.out.println("-------- 1. Swap ---------");
+		per1(arr, 0, n, 3);
 
-    System.out.println("\n-------- 2. DFS ---------");
-    per2(arr, output, visited, 0, n, 3); //r = 3, 3개를 뽑을 것 
-  }
+		System.out.println("\n-------- 2. DFS ---------");
+		per2(arr, output, visited, 0, arr.length, r); //r = 3, 3개를 뽑을 것 
+	}
 
 	//1. Swap 함수를 이용해 구현 - 순서 없이 n 개중에서 r 개를 뽑는 경우
 	static void per1(int[] arr, int depth, int n, int r) {
@@ -26,7 +26,7 @@ public class Main {
 		}
 	}
 
-  //두 배열의 값을 바꾸는 Swap 함수 
+	//두 배열의 값을 바꾸는 Swap 함수 
 	static void swap(int[] arr, int depth, int i) { 
 		int temp = arr[depth];
 		arr[depth] = arr[i];
