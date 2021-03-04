0291470e089acab5209d4788ea4f85b1eb261d99
Kwounsu Lee <sb00lgs@gmail.com>
2021-03-04 11:01:25 +0900

    Update SpyClothesCombination.py
    better and easier way

--- SpyClothesCombination.py
+++ SpyClothesCombination.py
@@ -1,43 +1,60 @@
 # import numpy as np
 
 def solution(clothes):
-    answer = 0
+    answer = 1
     # Count each categories
-    category = {}
-    for a in clothes:
-        if(a[1] in category):
-            category[a[1]] = category[a[1]]+1
+    categories = {}
+    for cloth, category in clothes:
+        if(category in categories):
+            categories[category] = categories[category]+1
         else:
-            category[a[1]] = 1
+            categories[category] = 1
 
-    if len(category) > 1:
-        # Convert category dict to the list
-        numOfClothes = []
-        for b in category:
-            numOfClothes.append(category[b])
-        # Get binary list
-        numOfCombination = 2**len(numOfClothes)
-        binaryList = []
-        for i in range(1,numOfCombination):
-            binary = []
-            for j in bin(i)[2:]:
-                binary.append(int(j))
-            while len(binary) < len(numOfClothes):
-                binary.insert(0,0)
-            binaryList.append(binary)
-        # Multiply category list and binary list
-        for i in binaryList:
-            multiplied = list(map(lambda x, y: x * y, numOfClothes, i))
-#             multiplied = np.array(numOfClothes)*np.array(i)
-#             multiplied = list([int(x) for x in multiplied])
-            comb = 1
-            for j in multiplied:
-                if j > 0:
-                    comb *= j
-            answer += comb
+    if len(categories) > 1:
+        for category in categories:
+            answer *= categories[category]+1
+        answer -= 1
     else:
         answer = len(clothes)
     return answer
+
+#     answer = 0
+#     # Count each categories
+#     category = {}
+#     for a in clothes:
+#         if(a[1] in category):
+#             category[a[1]] = category[a[1]]+1
+#         else:
+#             category[a[1]] = 1
+
+#     if len(category) > 1:
+#         # Convert category dict to the list
+#         numOfClothes = []
+#         for b in category:
+#             numOfClothes.append(category[b])
+#         # Get binary list
+#         numOfCombination = 2**len(numOfClothes)
+#         binaryList = []
+#         for i in range(1,numOfCombination):
+#             binary = []
+#             for j in bin(i)[2:]:
+#                 binary.append(int(j))
+#             while len(binary) < len(numOfClothes):
+#                 binary.insert(0,0)
+#             binaryList.append(binary)
+#         # Multiply category list and binary list
+#         for i in binaryList:
+#             multiplied = list(map(lambda x, y: x * y, numOfClothes, i))
+# #             multiplied = np.array(numOfClothes)*np.array(i)
+# #             multiplied = list([int(x) for x in multiplied])
+#             comb = 1
+#             for j in multiplied:
+#                 if j > 0:
+#                     comb *= j
+#             answer += comb
+#     else:
+#         answer = len(clothes)
+#     return answer
 
 if __name__ == '__main__':
     n = [["a", "1"], ["b", "1"], ["c", "1"], ["d", "2"], ["e", "2"], ["f", "3"], ["g", "3"]]
