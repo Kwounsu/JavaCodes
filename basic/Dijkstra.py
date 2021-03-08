9f59da5c96a22bf5724f0662210f160d601745ef
Kwounsu Lee <sb00lgs@gmail.com>
2021-03-08 15:57:49 +0900

    Create Dijkstra.py

---
+++ basic/Dijkstra.py
@@ -0,0 +1,21 @@
+# Dijkstra
+start = costs[0][0]
+d = [sys.maxsize for i in range(n)]
+d[start] = 0
+pq = [(start,0)]
+
+while len(pq) > 0:
+    print("pq",pq)
+    print("d",d)
+    top = pq.pop(0)
+    current = top[0]
+    distance = top[1]
+    if d[current] < distance:
+        continue;
+    for i in graph[current]:
+        nextIs = i[0]
+        nextDist = distance + i[1]
+        print("nextDist",nextDist)
+        if d[nextIs] > nextDist:
+            d[nextIs] = nextDist
+            pq.append((nextIs,nextDist))
